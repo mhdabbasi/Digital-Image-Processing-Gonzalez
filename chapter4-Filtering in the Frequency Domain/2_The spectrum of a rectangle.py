6f9136d1d60ccb698021d46d2e0a929e0927a8d7
mhdabbasi <mehdabasii@gmail.com>
2021-02-04 17:22:10 +0330

    updated

--- chapter4-Filtering in the Frequency Domain/2_The spectrum of a rectangle.py
+++ chapter4-Filtering in the Frequency Domain/2_The spectrum of a rectangle.py
@@ -3,22 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 img = cv2.imread('chapter4-Filtering in the Frequency Domain/images/Fig0423(a)(rectangle).tif',0)
-# print(img.shape) -> (1024,1024)
+
+#print(img.shape) -> (1024,1024)
 #img = cv2.resize(img,(49,49))
 def DFT(img) :
     spectrum = np.zeros_like(img)
     M,N = img.shape[:2]
+    # to shift DFT
+    for x in range(M):
+        for y in range(N):
+            img[x,y] = img[x,y]*(-1)**(x+y)
+
+    # compute DFT
     for u in range(M):
         for v in range(N):
             for x in range(M):
                 for y in range(N):
-                    img[x,y] = img[x,y]*(-1)**(x+y)
                     spectrum[x,y] += img[x,y] * np.exp(-2j*np.pi*(u*x/M+v*y/N))
-    return spectrum,time()
+
+    return spectrum
 
 spectrum = DFT(img)
 
-# so, it takes t(time to compute one value of spectrum[x,y])*M*M*N*N = 0.00021*1024**4 =64138 hour :-/
+# so, it takes t(time to compute one value of spectrum[x,y])*M*M*N*N = 0.00018*1024**4 =54975 hour :-/
 
 plt.imshow(spectrum,cmap='gray')
 plt.show()
