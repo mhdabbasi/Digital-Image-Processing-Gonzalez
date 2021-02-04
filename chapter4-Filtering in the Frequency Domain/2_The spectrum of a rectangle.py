3cf24aa83dd8a1ca75c134493bc4822e6ad3dd7c
mhdabbasi <mehdabasii@gmail.com>
2021-02-04 11:28:43 +0330

    2_The spectrum of a rectangle.py added

---
+++ chapter4-Filtering in the Frequency Domain/2_The spectrum of a rectangle.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+img = cv2.imread('chapter4-Filtering in the Frequency Domain/images/Fig0423(a)(rectangle).tif',0)
+# print(img.shape) -> (1024,1024)
+#img = cv2.resize(img,(49,49))
+def DFT(img) :
+    spectrum = np.zeros_like(img)
+    M,N = img.shape[:2]
+    for u in range(M):
+        for v in range(N):
+            for x in range(M):
+                for y in range(N):
+                    img[x,y] = img[x,y]*(-1)**(x+y)
+                    spectrum[x,y] += img[x,y] * np.exp(-2j*np.pi*(u*x/M+v*y/N))
+    return spectrum,time()
+
+spectrum = DFT(img)
+
+# so, it takes t(time to compute one value of spectrum[x,y])*M*M*N*N = 0.00021*1024**4 =64138 hour :-/
+
+plt.imshow(spectrum,cmap='gray')
+plt.show()
