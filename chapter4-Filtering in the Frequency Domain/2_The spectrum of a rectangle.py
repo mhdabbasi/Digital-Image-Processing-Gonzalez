683f295ddfb4cfa471cf5f3aafa2dfa702bafd2c
mhdabbasi <mehdabasii@gmail.com>
2021-02-06 18:24:35 +0330

    updated

--- chapter4-Filtering in the Frequency Domain/2_The spectrum of a rectangle.py
+++ chapter4-Filtering in the Frequency Domain/2_The spectrum of a rectangle.py
@@ -7,7 +7,7 @@ img = cv2.imread('chapter4-Filtering in the Frequency Domain/images/Fig0423(a)(r
 #print(img.shape) -> (1024,1024)
 #img = cv2.resize(img,(49,49))
 def DFT(img) :
-    spectrum = np.zeros_like(img)
+    spectrum = np.zeros_like(img,dtype=complex)
     M,N = img.shape[:2]
     # to shift DFT
     for x in range(M):
@@ -21,11 +21,15 @@ def DFT(img) :
                 for y in range(N):
                     spectrum[x,y] += img[x,y] * np.exp(-2j*np.pi*(u*x/M+v*y/N))
 
-    return spectrum
+    return np.abs(spectrum)
 
-spectrum = DFT(img)
+# dft = DFT(img)
 
 # so, it takes t(time to compute one value of spectrum[x,y])*M*M*N*N = 0.00018*1024**4 =54975 hour :-/
+# BUT,i can use cv2.dft that compute dft of an signal . it use fast fourier transform ,so it's very fast.
+dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
+dft = np.fft.fftshift(dft)        # to shift dft to center.
+dft = np.sqrt(dft[:,:,0]**2+dft[:,:,1]**2)
 
-plt.imshow(spectrum,cmap='gray')
+plt.imshow(dft,cmap='gray')
 plt.show()
