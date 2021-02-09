04a8024152f67433fb19a5fb72a37ad9c1548790
mhdabbasi <mehdabasii@gmail.com>
2021-02-09 10:36:49 +0330

    4_low pass filters(ILPF GLPF BLPF).py added

---
+++ chapter4-Filtering in the Frequency Domain/4_low pass filters(ILPF GLPF BLPF).py
@@ -0,0 +1,60 @@
+import numpy as np 
+import cv2
+import matplotlib.pyplot as plt 
+
+img = cv2.imread('chapter4-Filtering in the Frequency Domain/images/Fig0441(a)(characters_test_pattern).tif',0)
+D0 = 20
+n = 2.5
+
+def DFT(img):
+    dft = np.fft.fft2(img)
+    dft = np.fft.fftshift(dft)
+    return dft
+
+def IDFT(dft):
+    idft = np.fft.ifft2(dft)
+    return np.abs(idft)
+
+# IDEAL LOWPASS FILTERS
+def ILBF(img,D0):
+    H = np.zeros_like(img)
+    P,Q = H.shape[:2]
+    for u in range(P):
+        for v in range(Q):
+            D = np.sqrt((u-P/2)**2 + (v-Q/2)**2)
+            H[u,v] = int(D <= D0)
+    result = IDFT(DFT(img) * H)
+    return result
+    
+
+# GAUSSIAN LOWPASS FILTERS
+def GLBF(img,D0):
+    H = np.zeros_like(img,dtype=np.float32)
+    P,Q = H.shape[:2]
+    for u in range(P):
+        for v in range(Q):
+            D = np.sqrt((u-P/2)**2 + (v-Q/2)**2)
+            H[u,v] = np.exp((-D**2) / (2*(D0**2)))
+    result = IDFT(DFT(img) * H)
+    return result
+
+# BUTTERWORTH LOWPASS FILTERS
+def BLBF(img,D0,n):
+    H = np.zeros_like(img,dtype=np.float32)
+    P,Q = H.shape[:2]
+    for u in range(P):
+        for v in range(Q):
+            D = np.sqrt((u-P/2)**2 + (v-Q/2)**2)
+            H[u,v] = 1 / (1+(D/D0)**(2*n))
+    result = IDFT(DFT(img) * H)
+    return result
+
+# show results
+fig = plt.figure(figsize=(10,10))
+titles = ['Orginal Image','ILBF','GLBF','BLBF']
+for i,image in enumerate([img ,ILBF(img,D0) ,GLBF(img,D0) ,BLBF(img,D0,n)]):
+    ax = fig.add_subplot(2,2,i+1)
+    ax.imshow(image,cmap='gray')
+    ax.set_title(titles[i])
+    ax.axis('off')
+plt.show()
